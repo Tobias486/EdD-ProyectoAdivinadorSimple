085c25b10f48c7f6147e96a1d0e5ae46ebedff15
Tobias486 <tmolinablanco@gmail.com>
2017-05-16 17:03:09 -0300

    Update EmptyListException.java

--- TDALista/EmptyListException.java
+++ TDALista/EmptyListException.java
@@ -1,4 +1,4 @@
-public class EmptyListException extends RuntimeException {
+public class EmptyListException extends Exception {
     public EmptyListException(String s){
         System.out.println(s);
     }
