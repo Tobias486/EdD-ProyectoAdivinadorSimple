9fe5225df5b5d8628a394dc9cd610aa799b7349e
Tobias486 <tmolinablanco@gmail.com>
2017-05-23 00:40:38 -0300

    Update ArbolBinario.java

--- TDAArbolBinario/ArbolBinario.java
+++ TDAArbolBinario/ArbolBinario.java
@@ -1,6 +1,6 @@
 package TDAArbolBinario;
 
-// TODO: por quÈ usamos BTPosition??? Los tipos est·ticos en ArbolBinario deben ser BTPosition o BTNodo?
+// TODO: por qu√© usamos BTPosition??? Los tipos est√°ticos en ArbolBinario deben ser BTPosition o BTNodo?
 
 public class ArbolBinario<E> implements BinaryTree<E> {
 
@@ -45,7 +45,7 @@ public class ArbolBinario<E> implements BinaryTree<E> {
 	}
 	
 	private BTNodo<E> checkPosition(Position<E> p) throws InvalidPositionException {
-		if (p == null) //TODO: chequear lista vacÌa?
+		if (p == null) //TODO: chequear lista vac√≠a?
 			throw new InvalidPositionException();
 		try {
 			return (BTNodo<E>) p;
@@ -83,5 +83,97 @@ public class ArbolBinario<E> implements BinaryTree<E> {
 		
 		return result;
 	}
+	
+	public Position<E> left(Position<E> v) throws InvalidPositionException, BoundaryViolationException {
+		BTNodo<E> ver = checkPosition(v);
+		if(ver.getLeft() == null)
+			throw new BoundaryViolationException();
+		return ver.getLeft();
+    	}
+    
+	public Position<E> right(Position<E> v) throws InvalidPositionException, BoundaryViolationException {
+		BTNodo<E> ver = checkPosition(v);
+		if(ver.getRight() == null)
+			throw new BoundaryViolationException();
+		return ver.getRight();
+	}
+	
+	public boolean hasLeft(Position<E> v) throws InvalidPositionException {
+		BTNodo<E> ver = checkPosition(v);
+		return ver.getLeft() != null;
+	}	
+
+	public boolean hasRight(Position<E> v) throws InvalidPositionException {
+		BTNodo<E> ver = checkPosition(v);
+		return ver.getRight() != null;
+	}
+
+	public Position<E> addLeft(Position<E> v, E r) throws InvalidOperationException, InvalidPositionException {
+		BTNodo<E> ver = checkPosition(v);
+		if(ver.getLeft() != null)
+			throw new InvalidOperationException();
+		ver.setLeft(new BTNodo<E>(r, ver));
+		size++;
+		return ver.getLeft();
+	}
+
+	public Position<E> addRight(Position<E> v, E r) throws InvalidOperationException, InvalidPositionException {
+		BTNodo<E> ver = checkPosition(v);
+		if(ver.getRight() != null)
+		    throw new InvalidOperationException();
+		ver.setRight(new BTNodo<E>(r, ver));
+		size++;
+		return ver.getRight();
+	}
+
+	public void Attach(Position<E> v, BinaryTree<E> T1, BinaryTree<E> T2) throws InvalidPositionException {
+		BTNodo<E> ver = checkPosition(v);
+		BTNodo<E> meter;
+		try {
+			if(!isExternal(ver))
+				throw new InvalidPositionException();
+			if(!T1.isEmpty()){
+				ver.setLeft((BTNodo<E>) T1.root());
+				meter = (BTNodo<E>) T1.root(); meter.setParent(ver);
+			}
+			if(!T2.isEmpty()){
+				ver.setRight((BTNodo<E>) T2.root());
+				meter = (BTNodo<E>) T2.root(); meter.setParent(ver);
+			}
+		} 
+		catch(ClassCastException e) {throw new InvalidPositionException();}
+		catch(EmptyTreeException e){};
+		size+= T1.size() + T2.size();
+	}
+	
+	public Iterator<E> iterator(){
+		return new BTIterator<E>(this);
+	}
+
+	private void DFS(Position<E> v, Lista<Position<E>> a) throws InvalidPositionException{
+		a.addLast(v);
+		try{
+			if(hasLeft(v)) DFS(right(v),a);
+			if(hasRight(v)) DFS(left(v),a);
+	  	}
+		catch(BoundaryViolationException e){}
+	}
+
+	public Iterable<Position<E>> children(Position<E> v)throws InvalidPositionException{
+		BTNodo poner = checkPosition(v);
+		Lista<Position<E>> a = new Lista<Position<E>>();
+		a.addLast(poner);
+		DFS(v,a);
+		return a;
+	}
+	public Iterable<Position<E>> positions(){
+		Lista<Position<E>> list = new Lista<Position<E>>();
+		Position<E> a;
+		try{if(!isEmpty()){ a = root(); DFS(a,list);}}
+		catch(EmptyTreeException e) {}
+		catch(InvalidPositionException e){}
+
+		return list;
+	}  
 	
 }
