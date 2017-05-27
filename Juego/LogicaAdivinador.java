50e66fd2282bd9578d690efce56b747b3307ccb0
Tobias486 <tmolinablanco@gmail.com>
2017-05-27 17:19:56 -0300

    Update LogicaAdivinador.java

--- Juego/LogicaAdivinador.java
+++ Juego/LogicaAdivinador.java
@@ -6,14 +6,18 @@ public class LogicaAdivinador {
     BinaryTree<String> arbol;
     Position<String> pp;
     
+    int objetos;
+    int preguntas;
+    
     public LogicaAdivinador(){
         arbol = new ArbolBinario<String>();
+        objetos = 1;
+        preguntas = 0;
         try {
             pp = arbol.createRoot("una guitarra");
-            // pp = arbol.root();
         } catch (InvalidOperationException e){}
-        //catch(EmptyTreeException e){}
     }
+    
     
     public void siguientePregunta(boolean resp) throws BoundaryViolationException{
         try {
@@ -68,5 +72,59 @@ public class LogicaAdivinador {
         }
         catch(InvalidPositionException e){}
         catch(InvalidOperationException e){}
+        objetos++;
+        preguntas++;
     }
+    
+    public int cantidadObjetos(){
+    	return objetos;
+    }
+    public int cantidadPreguntas(){
+    	return preguntas;
+    }
+    public Stack<Position<String>> listaInternos(){
+    	Iterable<Position<String>> nodosArbol = arbol.positions();
+    	Stack<Position<String>> internos = new Pila<Position<String>>();
+
+    	Iterator<Position<String>> it = nodosArbol.iterator();
+    	Position<String> meterP;
+    	
+    	try{
+	    	while(it.hasNext()){
+	    		meterP = it.next();
+	    		
+		    	if(arbol.isInternal(meterP))
+		    		internos.push(meterP);
+    		}
+    	}
+    	catch(InvalidPositionException e){}
+    	catch(NoSuchElementException e){}
+ 
+    	return internos;
+    }
+    private String auxEliminar(Position<String> p){
+    	String s;
+    	s = p.element();
+    	try{
+	    	if(arbol.hasRight(p)){
+	    		s = auxEliminar(arbol.right(p));
+	    		arbol.remove(arbol.right(p));
+	    		
+	    	}
+	    	if(arbol.hasLeft(p)){
+	    		s = auxEliminar(arbol.left(p));
+	    		arbol.remove(arbol.left(p));
+	    	}
+	    	arbol.replace(p,s);
+    	}
+    	catch(InvalidPositionException e){}
+    	catch(InvalidOperationException e){}
+    	catch(BoundaryViolationException e){}
+    	return s;
+    }
+    
+    public void eliminarSubarbol(Position<String> p){
+    	auxEliminar(p);
+    }
+    
 }
